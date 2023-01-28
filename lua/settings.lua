ff51a6e43117277e377d9b35fec35a0433ca84f8
ctrljean <ctrljean@gmail.com>
2023-01-28 17:59:32 -0500

    agregando el soporte para caracteres UTF-8

--- lua/settings.lua
+++ lua/settings.lua
@@ -15,3 +15,4 @@ vim.wo.wrap = false -- evitar que el código salte a una nueva línea
 opt.hlsearch = false -- evitar que se haga un resaltado en las busquedas 
 opt.tabstop = 2 -- cantidad de tabs que doy o salto 2 espacios
 opt.shiftwidth = 2 -- este este el espacio que utiliza nvim para identar una línea
+opt.encoding="utf-8"
