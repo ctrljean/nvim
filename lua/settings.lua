4f51eb05640341a7878b1451bf148940f040279f
ctrljean <ctrljean@gmail.com>
2023-02-03 11:53:54 -0500

    copiar y pegar en construcción

--- lua/settings.lua
+++ lua/settings.lua
@@ -17,7 +17,8 @@ opt.tabstop = 2 -- cantidad de tabs que doy o salto 2 espacios
 opt.shiftwidth = 2 -- este este el espacio que utiliza nvim para identar una línea
 opt.encoding="utf-8" -- esto establece la codigicación de los carácteres a UTF-8
 opt.cursorline = true -- esto me muestra una linea opaca atras de donde estoy escribiendo para saber donde se encuentra ubicado el puntero 
---vim.opt.clipboard = "unnamedplus"
-vim.api.nvim_set_option("clipboard", "unnamedplus")
---revisar esta opción de otra forma
-
+vim.opt.clipboard = "unnamedplus" -- esto me permite tener acceso a el portapapeles de nvim y también del sistema
+vim.cmd [[
+	vmap <C-c> "+y
+	vmap <C-v> "+p
+]]
