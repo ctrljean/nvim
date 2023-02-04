8d3602c92bca10994f630fe76279bc587b29661b
ctrljean <ctrljean@gmail.com>
2023-02-04 00:10:14 -0500

    Pasando la configuración de floaterm a lua

--- lua/plugins/floaterm.lua
+++ lua/plugins/floaterm.lua
@@ -1,6 +1,4 @@
-vim.cmd [[
-let g:floaterm_keymap_new    = '<F7>'
-let g:floaterm_keymap_prev   = '<F8>'
-let g:floaterm_keymap_next   = '<F9>'
-let g:floaterm_keymap_toggle = '<F12>'
-]]
+vim.g.floaterm_keymap_new = '<F7>'
+vim.g.floaterm_keymap_prev = '<F8>'
+vim.g.floaterm_keymap_next = '<F9>'
+vim.g.floaterm_keymap_toggle = '<F12>'
