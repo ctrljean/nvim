2d30332b6159f4ac39700373505ce28bd0f530b2
ctrljean <ctrljean@gmail.com>
2023-02-14 14:52:07 -0500

    agregando comprobaciones a las configuraciones

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,1 +1,5 @@
+local autopairs = pcall(require, "nvim-autopairs")
+if not autopairs then
+	print("This extension requires autopairs.nvim please run PackerInstall") 
+end
 require("nvim-autopairs").setup{}
